302bee5a2b33f2f1f8b0987cde9711bab95aaa74
jayudoye <joeludoye@gmail.com>
2024-04-10 21:12:32 -0700

    Python - Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A class with a fixed set of  attributes"""
+
+
+class LockedClass:
+    """A class with only one attribute"""
+    __slots__ = ['first_name']
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+This is the README.md file for the ALX project 0x09-python-everything_is_object
