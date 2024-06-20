526fc84c4c92ddaee1437c7339129b2b0e07d6c5
jayudoye <joeludoye@gmail.com>
2024-06-19 19:11:29 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const argumentNum = process.argv.slice(2);
 
 if (argumentNum.length === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argumentNum.length === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
