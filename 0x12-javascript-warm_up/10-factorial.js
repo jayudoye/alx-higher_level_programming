19bbf99b123690ef7be54c5b04b7dd5b448e66f7
jayudoye <joeludoye@gmail.com>
2024-06-21 19:48:02 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
-const computeFactorial = (n) => {
-  if (isNaN(n)) {
-    return 1; // Factorial of NaN is 1
-  }
+const process = require('process');
+const args = process.argv;
 
-  n = parseInt(n);
+let numFactorial;
 
-  if (n === 0 || n === 1) {
+function recursion (n) {
+  if (n === 1) {
     return 1;
   } else {
-    return n * computeFactorial(n - 1);
+    return n * recursion(n - 1);
   }
-};
+}
 
-const input = process.argv[2];
-
-const result = computeFactorial(input);
-console.log(result);
+if (args.length < 3 || isNaN(args[2])) {
+  console.log(1);
+} else {
+  numFactorial = recursion(args[2]);
+  console.log(numFactorial);
+}
