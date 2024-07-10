c23a78c1383b3af65d87bbc8e12d884f2740c1d3
jayudoye <joeludoye@gmail.com>
2024-07-09 21:29:29 -0700

    Python - Network #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""
-A script that takes in a URL, sends a request to the URL
-and displays the value of the variable X-Request-Id in the response header
-"""
+"""This script takes in a URL, sends a request to the
+URL and displays the value of the variable
+X-Request-Id in the response header"""
 import requests
-from sys import argv
+import sys
 
-url = argv[1]
+# Check if a URL is provided as a command-line argument
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
 
-if __name__ == "__main__":
+url = sys.argv[1]
+
+try:
     response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+
+    print(response.headers.get('X-Request-Id'))
+
+except requests.RequestException as e:
+    print(f"Error accessing the URL: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
