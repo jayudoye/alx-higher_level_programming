57b7f868a7e7bf941845868a78ba438c5737aa3c
jayudoye <joeludoye@gmail.com>
2024-07-30 18:18:01 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,6 +7,7 @@ const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-	if (err) {
-		console.error(err)};
+  if (err) {
+    console.error(err);
+  }
 });
