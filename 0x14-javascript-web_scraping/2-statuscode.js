33845ce01ac81e4a6f56864ba025a90dc440e98b
jayudoye <joeludoye@gmail.com>
2024-07-30 19:22:09 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,6 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('code: ', response.statusCode);
-  }
+request.get(url).on('response', (response) => {
+  console.log('code: ', response.statusCode);
 });
